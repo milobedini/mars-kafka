6debd0203fc02c5e27ddf1f0bdd42911d3d5de16
Milo Bedini <milobedini64@gmail.com>
2024-01-17 16:36:24 +0000

    initial tests for turnRight

--- index.spec.ts
+++ index.spec.ts
@@ -5,6 +5,13 @@ const turnLeft = (x: string) => {
   if (x === 'E') return 'N'
   return 'N'
 }
+const turnRight = (x: string) => {
+  if (x === 'N') return 'E'
+  if (x === 'W') return 'N'
+  if (x === 'S') return 'W'
+  if (x === 'E') return 'S'
+  return 'N'
+}
 // test('When facing N, turning left should cause us to face W', () => {
 //   expect(turnLeft('N')).toBe('W')
 // })
@@ -22,5 +29,18 @@ test.each`
   'When facing $original, turning left should cause us to face $expected',
   ({ original, expected }) => {
     expect(turnLeft(original)).toBe(expected)
+  }
+)
+
+test.each`
+  original | expected
+  ${'N'}   | ${'E'}
+  ${'W'}   | ${'N'}
+  ${'S'}   | ${'W'}
+  ${'E'}   | ${'S'}
+`(
+  'When facing $original, turning right should cause us to face $expected',
+  ({ original, expected }) => {
+    expect(turnRight(original)).toBe(expected)
   }
 )
