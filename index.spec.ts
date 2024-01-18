9597d803b086026d6e36eaa997c8a4b77ea35548
Milo Bedini <milobedini64@gmail.com>
2024-01-18 14:45:42 +0000

    stage 1 complete

--- index.spec.ts
+++ index.spec.ts
@@ -1,17 +1,43 @@
-const turnLeft = (x: string) => {
-  if (x === 'N') return 'W'
-  if (x === 'W') return 'S'
-  if (x === 'S') return 'E'
-  if (x === 'E') return 'N'
-  return 'N'
+type Heading = 'N' | 'E' | 'S' | 'W'
+type Coordinates = [x: number, y: number]
+const Compass: Array<Heading> = ['N', 'E', 'S', 'W']
+
+const directions = {
+  // 1st element is left, 2nd element is right
+  N: ['W', 'E'],
+  W: ['S', 'N'],
+  S: ['E', 'W'],
+  E: ['N', 'S'],
 }
-const turnRight = (x: string) => {
-  if (x === 'N') return 'E'
-  if (x === 'W') return 'N'
-  if (x === 'S') return 'W'
-  if (x === 'E') return 'S'
-  return 'N'
+
+const rotate = (turns: number) => (heading: Heading) => {
+  const idx = Compass.indexOf(heading)
+  return Compass[(idx + turns) % 4]
 }
+
+const turnLeft =
+  // if (x === 'N') return 'W'
+  // if (x === 'W') return 'S'
+  // if (x === 'S') return 'E'
+  // if (x === 'E') return 'N'
+  // return 'N'
+  // const idx = Compass.indexOf(heading)
+  // return Compass[(idx + 3) % 4]
+  // So if we are facing N, we want to turn left to face W, this works because the left direction is always the next element in the array
+  rotate(3)
+
+const turnRight =
+  // initial hardcoded green
+  // if (x === 'N') return 'E'
+  // if (x === 'W') return 'N'
+  // if (x === 'S') return 'W'
+  // if (x === 'E') return 'S'
+  // return 'N'
+  // const idx = Compass.indexOf(heading)
+  // return Compass[(idx + 1) % 4]
+  rotate(1)
+
+// Initial red test setup
 // test('When facing N, turning left should cause us to face W', () => {
 //   expect(turnLeft('N')).toBe('W')
 // })
@@ -19,6 +45,18 @@ const turnRight = (x: string) => {
 // test('When facing W, turning left should cause us to face S', () => {
 //   expect(turnLeft('W')).toBe('S')
 // })
+
+const move = (heading: Heading, position: Coordinates) => {
+  const [x, y] = position
+  // Initial simple green
+  if (heading === 'N') return [x, y + 1]
+  if (heading === 'E') return [x + 1, y]
+  if (heading === 'S') return [x, y - 1]
+  if (heading === 'W') return [x - 1, y]
+}
+
+// ************ TESTS ************
+
 test.each`
   original | expected
   ${'N'}   | ${'W'}
@@ -44,3 +82,16 @@ test.each`
     expect(turnRight(original)).toBe(expected)
   }
 )
+
+test('When moving N, we should increment Y by 1', () => {
+  expect(move('N', [1, 1])).toEqual([1, 2])
+})
+test('When moving E, we should increment X by 1', () => {
+  expect(move('E', [1, 1])).toEqual([2, 1])
+})
+test('When moving S, we should decrease Y by 1', () => {
+  expect(move('S', [1, 1])).toEqual([1, 0])
+})
+test('When moving W, we should decrease X by 1', () => {
+  expect(move('W', [1, 1])).toEqual([0, 1])
+})
